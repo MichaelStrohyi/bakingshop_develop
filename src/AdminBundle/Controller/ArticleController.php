3820860998ebcb61407ca5140e354aca2b7b8621
Michael Strohyi <michael.strohyi@gmail.com>
2017-03-09 22:36:48 +0200

    [Admin] article: create new article

--- src/AdminBundle/Controller/ArticleController.php
+++ src/AdminBundle/Controller/ArticleController.php
@@ -5,13 +5,18 @@ namespace AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AppBundle\Entity\Article;
+use AdminBundle\Form\ArticleType;
 
+/**
+ * @Route("/article")
+ */
 class ArticleController extends Controller
 {
     /**
-     * @Route("/article")
+     * @Route("/", name="admin_article_index")
      * @Template()
      */
     public function indexAction()
@@ -35,7 +40,112 @@ class ArticleController extends Controller
      **/
     public function createAction(Request $request)
     {
+        $article = new Article;
+        $form = $this->createArticleForm($article, $request);
+
+        if ($form->isValid()) {
+            $this->persistArticle($article);
+
+            return $this->redirectToRoute("admin_article_index");
+        }
+
         return [
+            'form' => $form->createView(),
         ];
     }
+
+    /**
+     * Update article
+     *
+     * @param  Article  $article
+     * @param  Request  $request
+     * 
+     * @return Template
+     * @author Michael Strohyi
+     *
+     * @Route("/{id}/edit", name="admin_article_edit", requirements={"id": "\d+"})
+     * @ParamConverter("article", class="AppBundle:Article")
+     * @Template()
+     **/
+    public function editAction(Article $article, Request $request)
+    {
+        $form = $this->createArticleForm($article, $request);
+
+        if ($form->isValid()) {
+            $this->persistArticle($article);
+
+            return $this->redirectToRoute("admin_article_index");
+        }
+        return [
+            'article' => $article,
+            'form' => $form->createView(),
+        ];
+    }
+
+    /**
+     * Delete given article
+     *
+     * @param  Article  $article
+     * @param  Request  $request
+     * 
+     * @return Template
+     * @author Michael Strohyi
+     *
+     * @Route("/{id}/delete", name="admin_article_delete")
+     * @ParamConverter("article", class="AppBundle:Article")
+     * @Template()
+     **/
+    public function deleteAction(Article $article, Request $request)
+    {
+        $form = $this->createFormBuilder([])->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $entity_manager = $this->getDoctrine()->getEntityManager();
+
+            $entity_manager->remove($article);
+            $entity_manager->flush();
+
+            return $this->redirectToRoute("admin_article_index");
+        }
+
+        return [
+            'article' => $article,
+            'form' => $form->createView(),
+        ];
+    }
+
+    /**
+     * Save given article into database
+     *
+     * @param  Article  $article
+     * 
+     * @return void
+     * @author Michael Strohyi
+     **/
+    private function persistArticle(Article $article)
+    {
+        $entity_manager = $this->getDoctrine()->getEntityManager();
+
+        $entity_manager->persist($article);
+        $entity_manager->flush();
+    }
+
+     /**
+     * Return form for create/edit article
+     *
+     * @param Article $article
+     * @param Request $request
+     *
+     * @return Form
+     * @author Michael Strohyi
+     **/
+    private function createArticleForm(Article $article, Request $request)
+    {
+            $form = $this->createForm(new ArticleType(), $article);
+            $form->handleRequest($request);
+
+            return $form;
+    }
+
 }
---
+++ src/AdminBundle/Form/ArticleType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Article;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('header', null, ['attr' => ['autocomplete' => 'off']])
+            ->add('url', null, ['attr' => ['autocomplete' => 'off']])
+            ->add('body', 'textarea')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Article',
+        ));
+    }
+
+
+    public function getName()
+    {
+        return 'admin_article';
+    }
+}
