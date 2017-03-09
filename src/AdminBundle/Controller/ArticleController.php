ac667041c37b6d7d62b0e1fee1facc0290bf0e4f
Michael Strohyi <michael.strohyi@gmail.com>
2017-03-09 19:42:14 +0200

    [Admin] article: added route for create article

--- src/AdminBundle/Controller/ArticleController.php
+++ src/AdminBundle/Controller/ArticleController.php
@@ -3,6 +3,7 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AppBundle\Entity\Article;
@@ -18,6 +19,23 @@ class ArticleController extends Controller
         $article_list = $this->getDoctrine()->getRepository("AppBundle:Article")->findAllByHeader();
         return [
             'article_list' => $article_list,
+        ];
+    }
+
+    /**
+     * Create new article
+     *
+     * @param  Request  $request
+     * 
+     * @return Template
+     * @author Michael Strohyi
+     *
+     * @Route("/new", name="admin_article_create")
+     * @Template()
+     **/
+    public function createAction(Request $request)
+    {
+        return [
         ];
     }
 }
