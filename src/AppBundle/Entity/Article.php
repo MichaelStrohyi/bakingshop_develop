333f431bf427219b78dfcb94c8c75382e997c0c4
Michael Strohyi <michael.strohyi@gmail.com>
2017-03-09 13:26:32 +0200

    [Admin] article: generated article structure

---
+++ src/AppBundle/Entity/Article.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="header", type="string", length=255)
+     */
+    private $header;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="blob")
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="blob")
+     */
+    private $body;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set header
+     *
+     * @param string $header
+     * @return Article
+     */
+    public function setHeader($header)
+    {
+        $this->header = $header;
+
+        return $this;
+    }
+
+    /**
+     * Get header
+     *
+     * @return string 
+     */
+    public function getHeader()
+    {
+        return $this->header;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Article
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     * @return Article
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string 
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+}
