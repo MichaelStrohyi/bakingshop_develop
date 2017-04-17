da74827145a85a8f3468db3c3986e00b48a87d6f
Mykola Martynov <mykola.martynov@hotmail.com>
2017-04-17 19:00:45 +0300

    [Pages]: create bundle for pages

---
+++ src/USPC/PageBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace USPC\PageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function dispatcherAction($slug)
+    {
+        // !!! stub
+    }
+}
---
+++ src/USPC/PageBundle/USPCPageBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace USPC\PageBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class USPCPageBundle extends Bundle
+{
+}
