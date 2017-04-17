5b73aa0342c0bb92457e2c1f3ccbdc8257d6cd57
Mykola Martynov <mykola.martynov@hotmail.com>
2017-04-17 19:15:04 +0300

    [Pages]: create page table

---
+++ app/DoctrineMigrations/Version20170417161415.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170417161415 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE page (id INT AUTO_INCREMENT NOT NULL, url TINYBLOB NOT NULL, type VARCHAR(20) NOT NULL, object_id INT NOT NULL, is_alias TINYINT(1) NOT NULL, INDEX search_url (url(100)), INDEX search_type_url (type, url(100)), INDEX search_type_object (type, object_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE page');
+    }
+}
---
+++ src/USPC/PageBundle/Entity/Page.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace USPC\PageBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Index;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Page
+ *
+ * @ORM\Table(indexes={
+ *     @Index(name="search_url", columns={"url"}),
+ *     @Index(name="search_type_url", columns={"type", "url"}),
+ *     @Index(name="search_type_object", columns={"type", "object_id"})
+ * })
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\PageRepository")
+ */
+class Page
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * @var string
+     *
+     * @ORM\Column("url", type="blob", length=100)
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column("type", type="string", length=20)
+     */
+    private $type;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column("object_id", type="integer")
+     */
+    private $object_id;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column("is_alias", type="boolean")
+     */
+    private $is_alias;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Page
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @ORM\PostLoad
+     *
+     * @return void
+     * @author Mykola Martynov
+     **/
+    public function transformLoadedData()
+    {
+        if (is_resource($this->url) && get_resource_type($this->url) == 'stream') {
+            $this->url = stream_get_contents($this->url, -1, 0);
+        }
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Page
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set objectId
+     *
+     * @param integer $objectId
+     *
+     * @return Page
+     */
+    public function setObjectId($objectId)
+    {
+        $this->object_id = $objectId;
+
+        return $this;
+    }
+
+    /**
+     * Get objectId
+     *
+     * @return integer
+     */
+    public function getObjectId()
+    {
+        return $this->object_id;
+    }
+
+    /**
+     * Set isAlias
+     *
+     * @param boolean $isAlias
+     *
+     * @return Page
+     */
+    public function setIsAlias($isAlias)
+    {
+        $this->is_alias = $isAlias;
+
+        return $this;
+    }
+
+    /**
+     * Get isAlias
+     *
+     * @return boolean
+     */
+    public function getIsAlias()
+    {
+        return $this->is_alias;
+    }
+
+    /**
+     * Get isAlias
+     *
+     * @return boolean
+     * @author Mykola Martynov
+     **/
+    public function isAlias()
+    {
+        return $this->getIsAlias();
+    }
+}
