5ea93b393b9b50c55b2922e35472febce34ccae8
Michael Strohyi <michael.strohyi@gmail.com>
2017-09-07 18:32:28 +0300

    [Admin] store: created routes for create/edit/delete store

--- src/AdminBundle/Controller/StoreController.php
+++ src/AdminBundle/Controller/StoreController.php
@@ -3,7 +3,9 @@
 namespace AdminBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AppBundle\Entity\Store;
 
@@ -22,6 +24,61 @@ class StoreController extends Controller
 
         return [
             'store_list' => $store_list,
+        ];
+    }
+
+    /**
+     * Create new store
+     *
+     * @param  Request  $request
+     * 
+     * @return Template
+     * @author Michael Strohyi
+     *
+     * @Route("/new", name="admin_store_create")
+     * @Template()
+     **/
+    public function createAction(Request $request)
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Edit store
+     *
+     * @param  Store  $store
+     * @param  Request  $request
+     * 
+     * @return Template
+     * @author Michael Strohyi
+     *
+     * @Route("/{id}/edit", name="admin_store_edit", requirements={"id": "\d+"})
+     * @ParamConverter("store", class="AppBundle:Store")
+     * @Template()
+     **/
+    public function editAction(Store $store, Request $request)
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Delete given store
+     *
+     * @param  Store  $store
+     * @param  Request  $request
+     * 
+     * @return Template
+     * @author Michael Strohyi
+     *
+     * @Route("/{id}/delete", name="admin_store_delete")
+     * @ParamConverter("store", class="AppBundle:Store")
+     * @Template()
+     **/
+    public function deleteAction(Store $store, Request $request)
+    {
+        return [
         ];
     }
 
