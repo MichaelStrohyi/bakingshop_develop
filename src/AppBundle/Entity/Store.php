1da2659fd3eea2548b62e1e4fe760bd2f0010ab8
Michael Strohyi <michael.strohyi@gmail.com>
2017-09-07 17:19:31 +0300

    [Admin] store: generated store structure

---
+++ src/AppBundle/Entity/Store.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * Store
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\StoreRepository")
+ * @UniqueEntity("name", message="Store with this name already exists")
+ * @ORM\HasLifecycleCallbacks
+ */
+class Store
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=40, nullable=false)
+     * @Assert\NotBlank
+     * @Assert\Length(min=3, max=40)
+     * @Assert\Regex(pattern="/^[\w\d\s[:punct:]]*$/")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="blob", nullable=false)
+     * @Assert\Url(
+     *    message = "The url '{{ value }}' is not a valid url",
+     * )
+     */
+    private $url;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="keywords", type="text", nullable=true)
+     */
+    private $keywords;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Store
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Store
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set keywords
+     *
+     * @param string $keywords
+     * @return Store
+     */
+
+    /**
+     * @ORM\PostLoad
+     *
+     * @return void
+     * @author Michael Strohyi
+     **/
+    public function transformLoadedData()
+    {
+        if (is_resource($this->url) && get_resource_type($this->url) == 'stream') {
+            $this->url = stream_get_contents($this->url, -1, 0);
+        }
+    }
+
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+
+        return $this;
+    }
+
+    /**
+     * Get keywords
+     *
+     * @return string 
+     */
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Store
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
