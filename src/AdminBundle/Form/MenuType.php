7d1b0490373403acb2a7022d4a96e318f2d25597
Michael Strohyi <michael.strohyi@gmail.com>
2017-11-22 23:08:32 +0200

    [Admin] menu: added type of menu (top/side/bottom)

--- src/AdminBundle/Form/MenuType.php
+++ src/AdminBundle/Form/MenuType.php
@@ -11,9 +11,25 @@ class MenuType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $choices = ['' => null];
+        foreach (Menu::getTypes() as $key => $value) {
+            $choices[$value] = strtolower($value);
+        }
         $builder
             ->add('name', null, ['attr' => ['autocomplete' => 'off']])
             ->add('header', null, ['attr' => ['autocomplete' => 'off']])
+            ->add('type', 'choice', [
+                'choices'  => $choices,
+                'choice_attr' => function($key, $val, $index) {
+                    $disabled = false;
+                    if ($key === null) {
+                        $disabled = true;
+                    }
+
+                    return $disabled ? ['disabled' => 'disabled'] : [];
+                },
+                'choices_as_values' => true,
+            ])
         ;
     }
 
