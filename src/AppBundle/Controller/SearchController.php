b167303a387e8814650bf6dc1c176de58aa653fc
Michael Strohyi <michael.strohyi@gmail.com>
2018-02-08 22:28:41 +0200

    [App] search: added live-search for amp-version

--- src/AppBundle/Controller/SearchController.php
+++ src/AppBundle/Controller/SearchController.php
@@ -5,19 +5,23 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class SearchController extends PageController
 {
     /**
-     * @Route("/search", name="search_ajax")
+     * @Route("/{prefix}search", name="search_ajax",
+     *     requirements={"prefix": "amp/|"},
+     *     defaults={"prefix": ""},
+     * )
      */
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, $prefix)
     {
-        if (!$request->isMethod('POST')) {
+        if (!$request->isMethod('POST') && empty($prefix)) {
             throw $this->createNotFoundException();
         }
 
-        $articles_all_count = $stores_all_count = $articles = $stores = null;
+        $articles_all_count = $stores_all_count = $articles = $stores = $json_res = null;
         $needle= trim(strip_tags(stripcslashes(htmlspecialchars($request->get('search-ajax')))));
 
         if (!empty($needle)) {
@@ -41,14 +45,18 @@ class SearchController extends PageController
         }
 
         $parameters = [
-            'articles' => $articles,
-            'stores' => $stores,
             'needle' => $needle,
             'articles_count' => $articles_all_count,
             'stores_count' => $stores_all_count,
         ];
 
-        return $this->render('AppBundle:Page:search.html.twig', $parameters);
+        if (empty($prefix)) {
+            $parameters['articles'] = $articles;
+            $parameters['stores'] = $stores;
+            return $this->render('AppBundle:Page:search.html.twig', $parameters);
+        }
+
+        return new Response($this->getJsonRes($articles, $stores, $needle, $articles_all_count, $stores_all_count, $prefix));
     }
 
     /**
@@ -85,5 +93,67 @@ class SearchController extends PageController
         }
 
         return empty($prefix) ? $this->render('AppBundle:Page:list.html.twig', $parameters) : $this->render('AppBundle:amp/Page:list.html.twig', $parameters);
+    }
+
+    /**
+     * Convert list of $articles and $stores into JSON-string for live-search of amp-version.
+     *
+     * @param array $articles
+     * @param array $stores
+     * @return string
+     * @author Michael Strohyi
+     **/
+    private function getJsonRes($articles, $stores, $needle, $articles_count, $stores_count, $prefix = null)
+    {
+        if (empty($articles) && empty($stores)) {
+            return '{"items":[]}';
+        }
+
+        $items = [];
+        if (!empty($articles)) {
+            $items[] = [
+                'url' => '/',
+                'name' => 'Results from Articles', 'class' => 'search-result-type disabled',
+            ];
+            foreach ($articles as $article) {
+                $items[] = [
+                    'url' => '/' . $prefix . ltrim($article->getUrl(), '/'),
+                    'name' => $article->getHeader(),
+                    'class' => 'result',
+                ];
+            }
+            if ($articles_count > count($articles)) {
+               $items[] = [
+                   'url' => $this->generateUrl('search_page', ['slug' => 'article', 'prefix' => $prefix, 'q' => $needle]),
+                   'name' => "... more results for '" . $needle . "'",
+                   'class' => 'search-result-more',
+               ]; 
+            }
+        }
+
+        if (!empty($stores)) {
+            $items[] = [
+                'url' => '/',
+                'name' => 'Results from Stores', 
+                'class' => 'search-result-type disabled',
+            ];
+            foreach ($stores as $store) {
+                $items[] = [
+                    'url' => '/' . trim($prefix, '/') . $store->getUrl(),
+                    'name' => $store->getName(),
+                    'class' => 'result',
+                ];
+            }
+
+            if ($stores_count > count($stores)) {
+               $items[] = [
+                   'url' => $this->generateUrl('search_page', ['slug' => 'store', 'prefix' => $prefix, 'q' => $needle]),
+                   'name' => "... more results for '" . $needle . "'",
+                   'class' => 'search-result-more'
+               ]; 
+            }
+        }
+
+        return json_encode(["items" => $items]);
     }
 }
